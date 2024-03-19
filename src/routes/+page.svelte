3fafe25ca0cc2138722f604d11935f284fc66016
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-03-19 16:50:41 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,20 @@
 <script>
-    let res, eng
-	let hun = {value: '---'}
-	async function xget(name) {
-        const response = await fetch("/api/getstate", {
+    let eng
+	let hun = {value : '---'}
+	async function post(name) {
+        const res = await fetch("/api/getstate", {
             method: 'POST', body: JSON.stringify({name}),
             headers: { 'content-type': 'application/json' }
         })
-        hun = (await response.json())[0]
+        hun = (await res.json())[0]
 	}
 	export let data
 </script>
 
 <h1>Szótár</h1>
-<select bind:value={eng} on:change={() => xget(eng)}>
-    <option selected>--- </option>
-    {#each data.data as state}
-    <option>{state.name}</option>
-    {/each}
+<select bind:value={eng} on:change={() => post(eng)}>
+    {#if hun.value=='---'}<option>--- </option>{/if}
+    {#each data.data as state}<option>{state.name}</option>{/each}
 </select>
 <div class="e"><b>{eng}</b>: <i>{hun.value}</i></div>
 
